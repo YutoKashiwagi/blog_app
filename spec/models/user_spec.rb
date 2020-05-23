db4197929a8514beaabe87974d9db2910a8cf027
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-05-23 14:15:21 +0900

    ユーザー登録機能実装

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ファクトリー' do
+    let(:user) { build(:user) }
+
+    example '有効なファクトリを持つこと' do
+      expect { user.save }.to change(User, :count).by(1)
+    end
+  end
+
+  describe 'バリデーション' do
+    let(:user) { build(:user) }
+
+    context 'name' do
+      subject { user.errors[:name] }
+
+      example 'presence: true' do
+        user.name = ''
+        user.valid?
+        is_expected.to include("can't be blank")
+      end
+
+      example 'length: { maximum: 20 }' do
+        user.name = 'a' * 21
+        user.valid?
+        is_expected.to include("is too long (maximum is 20 characters)")
+      end
+    end
+  end
 end
