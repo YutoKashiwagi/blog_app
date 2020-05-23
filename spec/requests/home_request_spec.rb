6c91ceb5766284b3eb7302681a37d49e68038bbf
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-05-23 14:14:33 +0900

    rubocop

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-
   describe "#home" do
     example "リクエストに成功すること" do
       get root_path
       expect(response).to have_http_status(:success)
     end
   end
-
 end
