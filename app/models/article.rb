ff10997e30ff67729c2972a61565d6012cf0ac11
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-05-24 22:04:17 +0900

    記事のアソシエーション

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+
+  validates :title,   presence: true, length: { maximum: 100 }
+  validates :content, presence: true, length: { maximum: 20000 }
 end
